6151b593e65dd732d5dd7219b212bd7806ab0e92
vinays <vinays@VinayMac.local>
2023-07-29 22:39:51 +0530

    first commit

---
+++ src/main/java/com/edu/bootflux/BootfluxApplication.java
@@ -0,0 +1,13 @@
+package com.edu.bootflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/bootflux/BootfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.bootflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
